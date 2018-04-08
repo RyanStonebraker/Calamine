7676eec14189febc03ce668d11b66de75ef0bb4a
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-04-08 13:47:38 -0800

    Adds amphitheatre block spawning and scaling When an object is removed from a block, it respawns. Also, scaling is fixed to match the size of the block it is occupied and when an object is removed from the block, it rescales to its original prefab size.

--- Assets/Amphitheatre Blocks/Scripts/AmphitheatreBlockController.cs
+++ Assets/Amphitheatre Blocks/Scripts/AmphitheatreBlockController.cs
@@ -5,23 +5,41 @@ using GameAbstracts;
 
 public class AmphitheatreBlockController : MonoBehaviour {
 
-  public GameObject blockIcon;
+  public GameObject sourceBlock;
+
+  private GameObject blockIcon;
+  public float scaleFactor = 0.8f;
+
+  private GameObject block;
+
+  private string currentName;
+
+  private double lastSpawnTime;
+
+  void loadPrefabBlock () {
+      blockIcon = Instantiate (sourceBlock, new Vector3(0,0,0), new Quaternion(0,0,0,0));
+      lastSpawnTime = Time.time;
+      Vector3 center = GetComponent<Renderer>().bounds.center;
+      Vector3 scaledShape = new Vector3(blockIcon.transform.localScale.x * scaleFactor, blockIcon.transform.localScale.y * scaleFactor, blockIcon.transform.localScale.z * scaleFactor);
+      blockIcon.transform.localScale = scaledShape;
+      block = Instantiate(blockIcon, center, sourceBlock.transform.rotation) as GameObject;
+      Bounds thisBound = GetComponent<Renderer>().bounds;
+      Bounds iconBound = block.GetComponent<Collider>().bounds;
+
+      Vector3 currSize = thisBound.max - thisBound.min;
+      Vector3 icoSize = iconBound.max - iconBound.min;
+
+      block.transform.localScale = block.transform.localScale *  Mathf.Max(currSize.x, currSize.y) / (Mathf.Max(icoSize.x, icoSize.y));
+      block.transform.localScale = new Vector3(block.transform.localScale.x * scaleFactor, block.transform.localScale.y * scaleFactor, block.transform.localScale.z * scaleFactor);
+      currentName = block.name;
+  }
   void Start () {
-    // If a GameObject is attached, turn off prefab block
-    if (blockIcon)
-            GetComponent<Renderer>().enabled = false;
+    loadPrefabBlock();
+  }
 
-    GameObject block = Instantiate(blockIcon, transform.position, transform.rotation) as GameObject;
-    Bounds thisBound = GetComponent<MeshRenderer>().bounds;
-    Bounds iconBound = block.GetComponent<MeshRenderer>().bounds;
-
-    Vector3 currSize = thisBound.max - thisBound.min;
-    Vector3 icoSize = iconBound.max - iconBound.min;
-
-    block.transform.localScale = block.transform.localScale * (Mathf.Max(currSize.x, currSize.y) / Mathf.Max(icoSize.x, icoSize.y));
-	
-	// Fix wrong location generation
-    blockIcon.GetComponent<GameBlock>().StartPosition = gameObject.transform.position;
-    blockIcon.GetComponent<GameBlock>().StartRotation = gameObject.transform.rotation;
+  void OnTriggerExit(Collider other) {
+    other.transform.localScale = sourceBlock.transform.localScale;
+    if (other.gameObject.name == currentName && Time.time - lastSpawnTime > 0.5)
+      loadPrefabBlock();
   }
 }
