2c82990aaa33b7621d3ee262d4ec29fade668d69
cjlasley <cjlasley@alaska.edu>
2018-04-09 00:32:03 -0800

    Imported my changes from my Working Copy.
    And it actually worked too!!! I can't believe it!

---
+++ Assets/CollinDungeon/CollinLoadSource.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class CALSourceFile
+{
+	public string sourceFile;
+	public string currentLine;
+	private StringReader lineGetter;
+
+	public void ReadSourceFile()
+	{
+		string sourcePath;
+		StreamReader fileReader;
+
+		sourcePath = "Assets/CollinDungeon/CalamineTest.txt";
+		fileReader = new StreamReader(sourcePath);
+
+		sourceFile = fileReader.ReadToEnd();
+		if (sourceFile == "") {
+			Debug.Log("Could not open source file!");
+			Application.Quit();
+		}
+		lineGetter = new StringReader(sourceFile);
+	}
+
+	public string GetNextLine()
+	{
+		currentLine = lineGetter.ReadLine();
+		if (currentLine == null) {
+			Debug.Log("End of file!");
+			return null;
+		}
+
+		return currentLine;
+	}
+}
