76873a826a7dae4b7020fb0fa3b57d991fccbfea
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-04-09 16:48:52 -0800

    Fixes unused delete and adds DrawingCanvas to scene

--- Assets/Amphitheatre Blocks/Scripts/UnusedDelete.cs
+++ Assets/Amphitheatre Blocks/Scripts/UnusedDelete.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class UnusedDelete : MonoBehaviour {
 	public int destroyDelay = 5;
+	public bool deleteTriggered = false;
 
 	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.name.Contains("Controller")) {
+		if (other.gameObject.name.Contains("Controller") && deleteTriggered) {
 			CancelInvoke("MaybeDestroy");
+			deleteTriggered = false;
 		}
 	}
 	void OnTriggerExit(Collider other) {
 		if (other.gameObject.name.Contains("Controller")) {
+			deleteTriggered = true;
 			Invoke("MaybeDestroy", destroyDelay);
 		}
 	}
