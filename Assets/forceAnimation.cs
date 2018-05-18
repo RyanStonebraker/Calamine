e4a0f248ceca16e82804861bbfbdd209e9e9f94a
unknown <orthogonaltrajectory@gmail.com>
2018-05-17 20:57:14 -0800

    Counted loop spawn and spinning loop animations
    1/2 done with the Calamine animation demo, we'll need to discus camera placement and aesthetics after the basic animations are complete.

--- Assets/forceAnimation.cs
+++ Assets/forceAnimation.cs
@@ -7,13 +7,20 @@ public class forceAnimation : MonoBehaviour {
     Animator animator;
 
     public GameObject bouncyBall;
+    public GameObject countedLoop;
     public Vector3 displacementFromFox = new Vector3(5f, 20f, 0f);
-    
+    public Vector3 displacementFromSteamVR = new Vector3(1.17f, -3.8f, -14.5f);
+
     // Use this for initialization
     void Start () {
         animator = this.GetComponent<Animator>();
 	}
 	
+    private void spawnCountLoop()
+    {
+        Instantiate(countedLoop, GameObject.Find("SteamVR").transform.position + displacementFromSteamVR, new Quaternion());
+    }
+
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.F))
@@ -29,6 +36,18 @@ public class forceAnimation : MonoBehaviour {
         {
             animator.SetTrigger("Flick");
             Instantiate(bouncyBall, transform.position + displacementFromFox, new Quaternion());
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            if (!animator.GetBool("SitStay"))
+            {
+                animator.SetTrigger("CountedLoop");
+                Invoke("spawnCountLoop", 2.5f);
+            }
+
+            animator.SetBool("SitStay", !animator.GetBool("SitStay"));
+            Debug.Log("F registed down");
         }
     }
 }
