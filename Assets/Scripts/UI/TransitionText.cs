cc41adfe16a4961491c6d4b189f7d2aaca6c35d4
unknown <orthogonaltrajectory@gmail.com>
2018-05-18 23:48:46 -0800

    Reorganizes animation scripts

---
+++ Assets/Scripts/UI/TransitionText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionText : MonoBehaviour {
+	
+    public void changeTextWithEffect()
+    {
+        
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
