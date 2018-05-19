4057f30d25a087917881a4263841a6130e4c689f
unknown <orthogonaltrajectory@gmail.com>
2018-05-18 23:39:47 -0800

    Loop body animated label

--- Assets/forceAnimation.cs
+++ Assets/forceAnimation.cs
@@ -8,7 +8,8 @@ public class forceAnimation : MonoBehaviour {
 
     public GameObject bouncyBall;
     public GameObject countedLoop;
-    public GameObject arrow;
+    public GameObject loopCounterObject;
+    public GameObject loopBodyObject;
     public Vector3 displacementFromFox = new Vector3(5f, 20f, 0f);
     public Vector3 displacementFromSteamVR = new Vector3(1.17f, -3.8f, -14.5f);
 
@@ -20,6 +21,22 @@ public class forceAnimation : MonoBehaviour {
     private void spawnCountLoop()
     {
         Instantiate(countedLoop, GameObject.Find("SteamVR").transform.position + displacementFromSteamVR, new Quaternion());
+    }
+
+    private void displayLabel(GameObject label, Vector3 position, Quaternion rotation)
+    {
+        animator.SetTrigger("Point");
+        GameObject arrowWithLabel = Instantiate(label, position, rotation);
+        Component[] animatorsInChildren = arrowWithLabel.GetComponentsInChildren<Animator>();
+
+        foreach (Animator animator in animatorsInChildren) // emphasize object with scale up animation
+        {
+            if (animator.name == "Label")
+            {
+                Debug.Log("Label found in " + animator);
+                animator.SetTrigger("Grow");
+            }
+        }
     }
 
 	// Update is called once per frame
@@ -52,21 +69,15 @@ public class forceAnimation : MonoBehaviour {
         }
 
         if (Input.GetKeyDown(KeyCode.R))
-        {
-            animator.SetTrigger("Point");
-            GameObject arrowWithLabel = Instantiate(arrow, 
-                                                   GameObject.Find("CountedLoop(Clone)").transform.position + new Vector3(-4.37991301f, 1.64609f, 8.6065f), 
-                                                   Quaternion.Euler(0,32.307f,0));
-            Component[] animatorsInChildren = arrowWithLabel.GetComponentsInChildren<Animator>();
+            displayLabel(loopCounterObject,
+                         GameObject.Find("CountedLoop(Clone)").transform.position + 
+                         new Vector3(-4.37991301f, 1.85409f, 8.6065f),
+                         Quaternion.Euler(0f, 32.307f, 0f));
 
-            foreach(Animator animator in animatorsInChildren)
-            {
-                if (animator.name == "Label")
-                {
-                    Debug.Log("Label found in " + animator);
-                    animator.SetTrigger("Grow");
-                }
-            }
-        }
+        if (Input.GetKeyDown(KeyCode.T))
+            displayLabel(loopBodyObject,
+                         GameObject.Find("CountedLoop(Clone)").transform.position +
+                         new Vector3(-0.44791301f, 0.94609f, 7.1945f),
+                         Quaternion.Euler(0f, 0f, 0f));
     }
 }
