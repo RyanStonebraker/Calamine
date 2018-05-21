5b256dfc3bee22daa8bde807307b3b2187df0b80
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-05-21 13:56:40 -0800

    Adds raw audio voice over and associates with arrow press
    The voice over audio has not been edited yet, but it is set up to dynamically respond to the new audio when it is editted.

--- Assets/Scripts/Animation/forceAnimation.cs
+++ Assets/Scripts/Animation/forceAnimation.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class forceAnimation : MonoBehaviour {
 
@@ -126,6 +127,11 @@ public class forceAnimation : MonoBehaviour {
         spawnedStartBlock.GetComponent<Animator>().SetTrigger("SpawnInTwitch");
     }
 
+    private void playVoiceRecording(string recExtension) {
+        gameObject.GetComponent<AudioSource>().clip = (AudioClip)AssetDatabase.LoadAssetAtPath("Assets/Audio/calamine_voiceOver " + recExtension + ".wav", typeof(AudioClip));
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+
     private void animateNextScene()
     {
         sceneNumber++;
@@ -133,23 +139,27 @@ public class forceAnimation : MonoBehaviour {
         {
             case 1:
                 tayAISpeechBubble.changeTextWithEffect("You can use loops to make part of your program happen more than once.");
+                playVoiceRecording("02");
                 break;
-    
             case 2:
                 tayAISpeechBubble.changeTextWithEffect("There are two types of loops in Calamine.");
+                playVoiceRecording("03");
                 break;
 
             case 3:
                 tayAISpeechBubble.changeTextWithEffect("The Counted Loop and the Check Loop");
+                playVoiceRecording("04");
                 Invoke("sitAndSpawnCountLoop", 1f);
                 break;
 
             case 4:
                 tayAISpeechBubble.changeTextWithEffect("In this lesson, we’ll only be talking about the Counted Loop.");
+                playVoiceRecording("05");
                 break;
 
             case 5:
                 tayAISpeechBubble.changeTextWithEffect("Let’s say we have a program that makes a ball bounce in the run area.");
+                playVoiceRecording("06");
                 Invoke("flickTailSpawnBall", 1.5f);
                 /**************************************************************/
                 /* ADD MAKE BALL w/ RED COLOR ARGUMENT ON DRAFTING BOARD HERE */
@@ -158,61 +168,75 @@ public class forceAnimation : MonoBehaviour {
 
             case 6:
                 tayAISpeechBubble.changeTextWithEffect("But we want a program that makes twenty balls bouncing around the run area.");
+                playVoiceRecording("07");
                 break;
 
             case 7:
                 tayAISpeechBubble.changeTextWithEffect("We could put twenty of these \"Make Ball\" blocks on the drafting board,");
+                playVoiceRecording("08");
                 break;
 
             case 8:
                 tayAISpeechBubble.changeTextWithEffect("but that would take a long time and make our program look messy.");
+                playVoiceRecording("09");
                 break;
 
             case 9:
                 tayAISpeechBubble.changeTextWithEffect("Luckily, we can do this really easily with a loop!");
+                playVoiceRecording("10");
                 Invoke("spawnInteractableCountLoop", 1.2f);
                 tayAISpeechBubble.changeTextWithEffect("Go ahead and put this Counting Loop onto the drafting board.");
+                playVoiceRecording("11");
                 break;
 
             case 10:
                 tayAISpeechBubble.changeTextWithEffect("Now that we have our loop on the board, we need to tell the loop how many times it should repeat.");
+                playVoiceRecording("12");
                 break;
 
             case 11:
                 tayAISpeechBubble.changeTextWithEffect("Increase that 1 on the loop to 20; this is called the loop counter.");
+                playVoiceRecording("13");
                 Invoke("spawnLoopCounterLabel", 1f);
                 break;
 
             case 12:
                 tayAISpeechBubble.changeTextWithEffect("Good!");
+                playVoiceRecording("14");
                 break;
 
             case 13:
                 tayAISpeechBubble.changeTextWithEffect("Right now our loop is empty.");
+                playVoiceRecording("15");
                 break;
 
             case 14:
                 tayAISpeechBubble.changeTextWithEffect("We need to tell the loop what it needs to do twenty times.");
+                playVoiceRecording("16");
                 break;
 
             case 15:
                 tayAISpeechBubble.changeTextWithEffect("Put this \"Make Ball\" block into the empty slot on the loop.");
+                playVoiceRecording("17");
                 Invoke("spawnMakeBallBlock", 1f);
                 break;
 
             case 16:
                 tayAISpeechBubble.changeTextWithEffect("This is called the \"Loop Body.\"");
+                playVoiceRecording("18");
                 Invoke("spawnLoopBodyLabel", 1f);
                 break;
 
             case 17:
                 tayAISpeechBubble.changeTextWithEffect("Fantastic! Now all we need to do is run the program!");
+                playVoiceRecording("19");
                 Invoke("spawnStartBlock", 1f);
                 /* Run button appears and 20 balls bounce in run scene */
                 break;
 
             case 18:
                 tayAISpeechBubble.changeTextWithEffect("Now write a program on your own that makes 2 foxes in the run area.");
+                playVoiceRecording("20");
                 break;
         }
     }
@@ -238,6 +262,6 @@ public class forceAnimation : MonoBehaviour {
             spawnMakeBallBlock();
 
         if (Input.GetKeyDown(KeyCode.RightArrow))
-            animateNextScene();     
+            animateNextScene();
     }
 }
