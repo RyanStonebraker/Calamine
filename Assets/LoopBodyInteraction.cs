e261cb3d2e57c6ff1ac33f9597b94bbae8cb66de
unknown <orthogonaltrajectory@gmail.com>
2018-05-22 17:06:11 -0800

    Loop body can hold objects tagged 'Statement'
    This only works when the object is in hand, there's an issue with it not working on the drafting board that I am currently looking into

---
+++ Assets/LoopBodyInteraction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoopBodyInteraction : MonoBehaviour {
+
+    public Text bodyText;
+    public Material occupiedBodyMat;
+    private bool lerp = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Statement")
+        {
+            bodyText.text = other.name;
+            lerp = true;
+            gameObject.GetComponent<Renderer>().material = occupiedBodyMat;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if(lerp)
+            bodyText.color = Color.Lerp(Color.blue, Color.yellow, Mathf.PingPong(Time.time, 1));
+    }
+}
---
+++ Assets/Scripts/WhoIsTrigger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhoIsTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name + " entered " + gameObject.name);
+    }
+}
